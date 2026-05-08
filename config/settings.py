a355407dff0548f70e3b15b6d812a55fa3bcdde5
ZhuYu <gluweinzhu@hotmail.com>
2026-05-09 01:05:19 +0800

    chore: bump version to 1.2.0

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,5 @@
 ############################################################
-version = "1.1.1"
+version = "1.2.0"
 author = "breadman"
 email = "gluweinzhu@hotmail.com"
 description = "A local RAG system"
